32a06db8d95cef493d8729253d8111a8632bb3e3
duduzeradev <144742621+duduzeradev@users.noreply.github.com>
2024-11-26 22:37:53 -0300

    asd

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,21 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    "Programação",
+    "O que é HTML?",
+    "HTML é uma linguagem de marcação."
 )
-
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    "Geografia",
+    "Qual a diferença entre tempo e clima?",
+    "Tempo é a condição atmosférica em um local específico em um dado momento. Enquanto o tempo pode mudar rapidamente, o clima é mais estável e característico de uma área.",
 )
-
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    "Historia",
+    "Qual foi a principal causa da Primeira Guerra Mundial?",
+    "A principal causa da Primeira Guerra Mundial foi o sistema de alianças entre países europeus, agravado por nacionalismo, imperialismo e rivalidades econômicas.",
 )
-
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    "Historia",
+    " Qual é o maior rio do mundo em volume de água?",
+    "O maior rio do mundo em volume de água é o Rio Amazonas",
 )
+let container = document.gerElementCreateById('container')
